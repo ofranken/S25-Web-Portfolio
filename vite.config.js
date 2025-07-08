68d135b9079d42185737f4187a42e25a8c16811f
Olivia Franken <89462842+ofranken@users.noreply.github.com>
2025-07-08 12:11:09 -0500

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
+  base: '/s25-web-portfolio/'
 })
